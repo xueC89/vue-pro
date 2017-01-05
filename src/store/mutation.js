4f4228572e985b9527b195ab70c60d03a31cbada
biancheng <446450825@qq.com>
2017-01-05 21:20:02 +0800

    add new file

---
+++ src/store/mutation.js
@@ -0,0 +1,23 @@
+
+/**
+ * 公共
+ */
+
+export const COM_LOADING_STATUS = 'COM_LOADING_STATUS'
+export const COM_LOGIN_STATUS = 'COM_LOGIN_STATUS'
+
+
+/**
+ * 首页
+ */
+
+export const INDEX_GET_PICS = 'INDEX_GET_PICS'
+export const INDEX_GET_LIST = 'INDEX_GET_LIST'
+
+
+/**
+ * 个人中心
+ */
+
+export const CENTER_USER_NAME = 'CENTER_USER_NAME'
+export const CENTER_USER_AVATAR = 'CENTER_USER_AVATAR'
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex';
+
+Vue.use(Vuex)
+
+const store = new Vuex.store({
+    modules: {
+
+    },
+    strict: process.env.NODE_ENV !== 'production' //是否开启严格模式
+})
+
+export default store
